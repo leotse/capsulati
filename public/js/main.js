6d556a5ac2afb6ea0dddc48f022be428deefac2a
Leo Tse <leocltse@gmail.com>
2016-02-14 18:17:53 -0500

    Created a simple virtual carousel based on bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,62 @@
 (function() {
   'use strict';
 
-  $(document).ready(onDocReady);
+  // internal control states
+  var animating = false;
+  var index = 0;
+  var size = 3;
 
-  function onDocReady() {
-    $('#main').fullpage({
-      autoScrolling: true,
-      loopTop: true,
-      loopBottom: true
+  // internal dom refs
+  var $carousel, $prev, $next, $items, $active;
+
+  // init control when doc ready!
+  $(document).ready(initCarousel);
+
+  // helper - prev slide
+  function prev() {
+    if(animating ||index === 0) { return; }
+    animating = true;
+    index--;
+    $items[index % size].innerText = 'item ' + index;
+    $carousel.carousel('prev');
+  }
+
+  // helper - next slide
+  function next() {
+    if(animating) { return; }
+    animating = true;
+    index++;
+    $items[index % size].innerText = 'item ' + index;
+    $carousel.carousel('next');
+  }
+
+  // helper- init carousel
+  function initCarousel() {
+
+    // init dom refs
+    $prev = $('#prev');
+    $next = $('#next');
+    $carousel = $('#carousel');
+    $items = $('#carousel .carousel-inner .item');
+    $active = $('#carousel .carousel-inner .item.active');
+
+    // init internal states
+    size = $items.size();
+    index = $active.index();
+    $active.text('item ' + index);
+
+    // init carousel
+    $carousel.carousel({
+      interval: 0,
+      keyboard: true
     });
 
-    var count = 5;
-    var timer = setInterval(add, 2000);
+    $carousel.on('slid.bs.carousel', function() {
+      animating = false;
+    });
 
-    function add() {
-      $('#main').append('<div class="section">Section wer</div>');
-      $.fn.fullpage.reBuild();
-    }
+    // init carousel controls
+    $prev.on('click', prev);
+    $next.on('click', next);
   }
 }());
