2cfb9cfd2fa631e5fdccf7ae89114eabb17c2954
Leo Tse <leocltse@gmail.com>
2016-05-08 18:04:03 -0400

    Updated instagram client to handle paging. Added function to page until a certain date

--- src/lib/api/instagram.js
+++ src/lib/api/instagram.js
@@ -4,29 +4,51 @@
 // creating this wrapper to take advantage for better non sandboxed data for testing
 
 // lib
+const _ = require('lodash');
 const request = require('request-promise');
 const log = require('lib/util/logger');
-
 const BASE_URL = 'https://api.instagram.com/v1';
 
 // an instagram request that supports paging
-class IGReq {
+const InstagramRequest = module.exports = class InstagramRequest {
+
+  static recentByTagUntil(tag, until, opts) {
+    return new Promise((resolve, reject) => {
+      const req = InstagramRequest.recentByTag(tag, opts);
+
+      // start paging instagram results
+      let all = [];
+      req.next().then(checkMore).catch(reject);
+
+      function checkMore(res) {
+        all = all.concat(res.data);
+        const media = _.last(res.data);
+        const lastUpdated = getLastUpdated(media);
+        if(req.hasMore && lastUpdated >= until) {
+          req.next().then(checkMore).catch(reject);
+        } else {
+          resolve(all);
+        }
+      }
+    });
+  }
 
   static recentByTag(tag, opts) {
     const url = `${BASE_URL}/tags/${tag}/media/recent`;
-    return new IGReq(url, opts);
+    return new InstagramRequest(url, opts);
   }
 
   constructor(url, opts) {
     this.paged = 0;
     this.nextUrl = null;
+
     this.url = url;
     this.count = opts.count;
     this.apiKey = opts.apiKey;
   }
 
   next() {
-    log('igreq:', this.url);
+    log('instagram request:', this.nextUrl || this.url);
     if(this.paged === 0) {
       const query = {
         client_id: this.apiKey,
@@ -43,7 +65,7 @@ class IGReq {
         json: true
       }).then(this._onPaged.bind(this));
     } else {
-      log('igreq: nothing to page');
+      log('instagram request: nothing to page');
       return Promise.resolve({ data: [] });
     }
   }
@@ -55,6 +77,37 @@ class IGReq {
   _onPaged(res) {
     this.nextUrl = res.pagination.next_url;
     this.paged++;
-    return res;
+    return parse(res);
   }
+};
+
+// helper - parse ig media
+function parse(res) {
+  res.data = res.data.map(d => {
+    d.created_time = new Date(d.created_time * 1000);
+    d.caption.created_time = new Date(d.caption.created_time * 1000);
+    d.comments.data = d.comments.data.map(c => {
+      c.created_time = new Date(c.created_time * 1000);
+      return c;
+    });
+    return d;
+  });
+  return res;
 }
+
+// helper - get last updated time of media
+// this is only an approximation since not all comments are returned
+function getLastUpdated(media) {
+  const times = [ media.created_time, media.caption.created_time ]
+    .concat(media.comments.data.map(c => c.created_time));
+  return _.max(times);
+}
+
+// debug code
+// const config = require('config');
+// InstagramRequest.recentByTagUntil('werwerwer', new Date('2016-01-01 00:00'), {
+//   count: 10,
+//   apiKey: config.instagram.clientid2
+// }).then(media => {
+//   console.log(media.length);
+// });
